de2eda357a6cb853c20e302e7c45f5bfd2b9d17a
Sameetha-java-developer <sameetha.java.developer@gmail.com>
2023-04-05 20:49:27 +0530

    Upload Operation Performed Successfully

---
+++ ABC.java
@@ -0,0 +1,16 @@
+class ABC{
+	public static void main(String[] args)
+{
+	char data='A';
+	for(int i=1;i<=26;i++){
+		for(int r=1;r<=i;r++){
+			for(int c=1;c<=i;c++){
+				System.out.println(data);
+			}
+			}
+			
+			data++;
+
+		}
+	}
+}
